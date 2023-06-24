ab681b62235b6f2d5b6ea8fec2fdf4f5653727e2
William S <will.step.205@gmail.com>
2023-06-24 12:32:15 -0700

    passing all tests on FCC

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,11 +3,13 @@ import './Editor.scss';
 // npm install --save marked // npm install --save @types/marked
 import { marked } from 'marked';
 
-let renderer = new marked.Renderer();
+marked.setOptions({
+  breaks: true,
+});
 
 export default function Editor() {
   const [text, setText] = useState({
-    text: ""
+    text: placeholder
   })
 
   function createMarkup() {
@@ -32,24 +34,74 @@ export default function Editor() {
   return (
     <div>
       <textarea
+        id="editor"
+        className='textarea-class'
         value={text.text}
         placeholder="text here"
         onChange={handleEdit}
         name="text"
       ></textarea>
 
-      <textarea
-        value={text.text}
-        placeholder="markup here"
-        onChange={handleEdit}
-        name="text"
-      ></textarea>
-
       <div
+        id="preview"
+        className='markdown-previewer'
         dangerouslySetInnerHTML={createMarkup()}
-        placeholder="markup here"
         onChange={handleEdit}
       ></div>
     </div>
   );
 }
+
+const placeholder = 
+`
+# Markdown Previewer
+
+Hi, I'm [William](https://github.com/will-s-205), welcome to my Markdown Previewer! 
+
+The project was built for learning purposes as a part of **Front End Libraries Certification** by [freeCodeCamp](https://www.freecodecamp.org/).
+
+You can find the challenge by clicking [here](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer).
+
+![freeCodeCamp](https://raw.githubusercontent.com/sitek94/pomodoro-clock/master/free-code-camp.png)
+
+## Marked - Markdown Parser
+[Marked] lets you convert [Markdown] into \`HTML\`.  Markdown is a simple text format whose goal is to be very easy to read and write, even when not converted to HTML.  This demo page will let you type anything you like and see how it gets converted.  Live.  No more waiting around.
+
+How To Use It?
+-------------------
+
+1. Type in stuff on the left.
+2. See the live updates on the right.
+
+That's it.  Pretty simple.
+
+Why Markdown?
+-------------
+
+It's easy.  It's not overly bloated, unlike HTML.  Also, as the creator of [markdown] says,
+
+> The overriding design goal for Markdown's
+> formatting syntax is to make it as readable
+> as possible. The idea is that a
+> Markdown-formatted document should be
+> publishable as-is, as plain text, without
+> looking like it's been marked up with tags
+> or formatting instructions.
+
+Ready to start writing? Then start changing stuff, have fun!
+
+[Marked]: https://github.com/markedjs/marked/
+[Markdown]: http://daringfireball.net/projects/markdown/
+
+\`\`\`
+// this is multi-line code:
+
+function passTests(code) {
+  if (code.includes(multiLineCode)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+\`\`\`
+`
