8a7b3a338ad426af913700baee5bf487c832b35e
William S <will.step.205@gmail.com>
2023-06-24 13:04:38 -0700

    simple workable version

--- src/components/Previewer.tsx
+++ src/components/Previewer.tsx
@@ -1,10 +1,40 @@
 import React, { useState } from 'react';
-import './Editor.scss';
+import './Previewer.scss';
 
 export default function Previewer() {
+    const [text, setText] = useState({
+        text: ''
+    })
+
+    const handleEdit: any = (event: {
+        target: {
+            value: any; name: any;
+        };
+    }) => {
+        setText(prevText => {
+            const { name, value } = event.target
+            console.log({ [event.target.name]: event.target.value }) // DEBUG
+            return {
+                ...prevText,
+                [name]: value
+            }
+        })
+    }
+
     return (
-        <div>
-            
+        <div className='editor'>
+            <textarea
+                className='textarea'
+                value={text.text}
+                onChange={handleEdit}
+                name="text"
+            ></textarea>
+            <textarea
+                className='textarea'
+                value={text.text}
+                onChange={handleEdit}
+                name="text"
+            ></textarea>
         </div>
     )
 }
