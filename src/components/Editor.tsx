197e7e968da681db7c61198c5ecccb52f78d0b50
William S <will.step.205@gmail.com>
2023-06-23 21:13:12 -0700

    add setText in textarea

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Editor.scss';
 
 export default function Editor() {
+  const [text, setText] = useState({
+    text: ""
+  })
+
+  const handleEdit: any = (event: {
+    target: {
+      value: any; name: any;
+    };
+  }) => {
+    setText(prevText => {
+      const {name, value} = event.target
+      console.log({[event.target.name]: event.target.value}) // DEBUG
+      return {
+        ...prevText,
+        [name]: value
+      }
+    })
+  }
+
   return (
     <div>
-<textarea></textarea>
-<textarea></textarea>
+      <textarea
+        value={text.text}
+        placeholder="Comments"
+        onChange={handleEdit}
+        name="text"
+      ></textarea>
+      <textarea></textarea>
     </div>
   );
 }
